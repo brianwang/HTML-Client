d2f74a379c94aa41713fc743d5709c16046377f0
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-28 01:14:18 -0400

    created ServerSelection and ServerOtherInput components for login scene

---
+++ src/scenes/login/components/ServerOtherInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServerOtherInput = () => (
+  <div class="form-group">
+    <div id="server_other">
+        <label for="server_other_input">{"Other Server:"}</label>
+        <input name="server_other_input" id="server_other_input" type="text" class="form-control" />
+    </div>
+  </div>
+)
+
+export default ServerOtherInput
---
+++ src/scenes/login/components/ServerSelection.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ServerOtherInput from './ServerOtherInput'
+
+const ServerSelection = () => (
+  <h4>{"Please log in to continue, or see below for instructions and assistance."}</h4>
+  <form name="login_information" id="login_information" class="login-form">
+    <div class="form-group">
+      <label for="server" aria-label="Server">{"Server:"}</label>
+      <select name="server" id="server" class="form-control">
+          <option value="ws://127.0.0.1:4243">{"Local host"}</option>
+          <option value="ws://dwarftowers.com:4243">{"dwarftowers.com"}</option>
+          <option value="ws://stats.zomis.net:4243">{"zomis.net"}</option>
+          <option value="other">{"Other..."}</option>
+      </select>
+      // TODO add some clarification as to what this "secure" selection actually does
+      <label for="secure">{"Is secure server:"}</label>
+      <input name="secure" id="secure" type="checkbox" value="secure" />
+    </div>
+
+    // TODO This part of the form should only be shown if `option value="other"` above is selected
+    <ServerOtherInput />
+
+    <div class="form-group">
+      <label for="username">{"Username:"}</label>
+      <input name="username" id="username" type="text" class="form-control" placeholder="Enter name..." />
+    </div>
+    <div class="form-group">
+      <input name="submit" id="submit" type="button" value="Log in" class="btn btn-success" />
+    </div>
+  </form>
+)
+
+export default ServerSelection
