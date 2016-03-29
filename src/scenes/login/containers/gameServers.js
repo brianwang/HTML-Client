5b7a61753c276e5222b1494fb6c0392f8d30f9a7
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2016-03-29 10:06:28 -0400

    fix unclosed string

--- src/scenes/login/containers/gameServers.js
+++ src/scenes/login/containers/gameServers.js
@@ -3,7 +3,7 @@ const gameServers = [
     href: "",
     websocketUri: "ws://127.0.0.1:4243",
     description: "Select this if you are hosting a local game server on your own machine." },
-  { name: "dwarftowers.com,
+  { name: "dwarftowers.com",
     href: "http://www.dwarftowers.com/",
     websocketUri: "ws://dwarftowers.com:4243",
     description: "Server hosted by our development team." },
