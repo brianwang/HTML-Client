b5399046e9b648c75783a1ab4360dd1d79923248
Francis Veilleux-Gaboury <Phrancis@users.noreply.github.com>
2016-03-29 09:42:20 -0400

    Fix errors and unnecessary escaping in GettingStarted.js
    As discussed in [this thread](https://github.com/Cardshifter/HTML-Client/commit/ac05cb54f3b6d60b22d892a198ab9a5e5e9c8a1a)

--- src/scenes/login/components/GettingStarted.js
+++ src/scenes/login/components/GettingStarted.js
@@ -1,33 +1,18 @@
 import React from 'react'
 
 const GettingStarted = () => (
-  <h3>{"Getting started"}</h3>
-
-  <p>{"To begin playing, first select the server you would like to connect to:"}</p>
-  <ul>
-    <li>{[
-      <strong>"Local Host: "</strong>, 
-      "Select this if you are hosting a local game server on your own machine."
-    ]}</li>
-    <li>{[
-      <strong>"dwarftowers.com: "</strong>, 
-      <a href="http://www.dwarftowers.com">"dwarftowers.com"</a>,
-      " server hosted by our development team."
-    ]}</li>
-    <li>{[
-      <strong>"zomis.net: "</strong>,
-      <a href="http://www.zomis.net/">"Zomis Productions"</a>
-      " server hosted by our development team."
-    ]}</li>
-    <li>{[
-      <strong>"Other: "</strong>,
-      " Enter a server address manually."
-    ]}</li>
-  </ul>
-  <p>{[
-      "Then enter a user name and click ", 
-      <strong>"Log in"</strong>"."
-  ]}</p>
+  <div>
+    <h3>Getting started</h3>
+  
+    <p>To begin playing, first select the server you would like to connect to:</p>
+    <ul>
+      <li><strong>Local Host: </strong>Select this if you are hosting a local game server on your own machine.</li>
+      <li><strong>dwarftowers.com: </strong><a href="http://www.dwarftowers.com">dwarftowers.com</a> server hosted by our development team.</li>
+      <li><strong>zomis.net: </strong><a href="http://www.zomis.net/">Zomis Productions</a> server hosted by our development team.</li>
+      <li><strong>Other: </strong> Enter a server address manually.</li>
+    </ul>
+    <p>Then enter a user name and click <strong>Log in</strong>.</p>
+  </div>
 )
 
 export default GettingStarted
